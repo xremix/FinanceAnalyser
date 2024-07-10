829b653cb835d89c51779b4cfed702d3a6979e75
xremix <tonidhoffmann@gmail.com>
2024-07-10 22:43:35 +0200

    parse lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ImportService } from './services/import-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'FinanceAnalyser';
+
+  constructor(private importService: ImportService) {}
+  file: any;
+  async fileChanged(e: any) {
+
+    this.file = e.target.files[0];
+let fileContent = await this.importService.getFileContent(this.file);
+let transactions = this.importService.parseCsvToTransactions(fileContent);
+console.log(transactions);
+    
+  }
+
 }
--- src/app/services/import-service.ts
+++ src/app/services/import-service.ts
@@ -4,14 +4,32 @@ import { Transaction } from '../models/transaction';
   providedIn: 'root',
 })
 export class ImportService {
+
+    public async getFileContent(file: File): Promise<string> {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+          resolve(fileReader.result as string);
+        };
+        fileReader.onerror = (error) => {
+          reject(error);
+        };
+        fileReader.readAsText(file);
+      });
+    }
+
+
   public parseCsvToTransactions(csvData: string): Transaction[] {
     const lines = csvData.split('\n');
     const transactions: Transaction[] = [];
     // Überspringe die Kopfzeile
     for (let i = 1; i < lines.length; i++) {
+
       const line = lines[i];
       if (line.trim() === '') continue; // Überspringe leere Zeilen
       const columns = line.split(';');
+      if(columns.length < 9) continue; // Überspringe Zeilen mit zu wenig Spalten
+      console.log(columns)
       const transaction: Transaction = {
         bookingDate: new Date(columns[0].split('.').reverse().join('-')), // buchung
         valueDate: new Date(columns[1].split('.').reverse().join('-')), // valuta
