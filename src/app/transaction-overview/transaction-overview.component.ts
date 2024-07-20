8f347928233d1075f580797e7005c52eca77175c
xremix <tonidhoffmann@gmail.com>
2024-07-20 13:26:11 +0200

    monthly reoccuring

--- src/app/transaction-overview/transaction-overview.component.ts
+++ src/app/transaction-overview/transaction-overview.component.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
 import { DataState } from '../services/data-state';
-import { TransactionComponentComponent } from '../transaction-component/transaction-component.component';
+import { Transaction } from '../models/transaction';
 
 @Component({
   selector: 'app-transaction-overview',
   templateUrl: './transaction-overview.component.html',
-  styleUrl: './transaction-overview.component.scss'
+  styleUrl: './transaction-overview.component.scss',
 })
 export class TransactionOverviewComponent {
-  protected expanded: boolean = false;
-constructor(
-  protected dataState: DataState
-) { }
+  protected expanded = {
+    all: false,
+    reoccurent: false,
+  };
+  constructor(protected dataState: DataState) {}
+  duplicateTransactions: Transaction[] = [];
+  findDuplicateTransactions() {
+    // check where amount and payer/receiver are the same
+    // always just return the first of the similar transactions
+    const duplicates = [];
+    const transactions = this.dataState.selectedTransactions;
+    for (let i = 0; i < transactions.length; i++) {
+      console.log('checking', transactions[i]);
+      if(this.foundDuplicates(transactions[i], transactions)  > 3) {
+        
+        // check if is already in duplicates
+        if(this.foundDuplicates(transactions[i], duplicates) == 0) {
+
+          duplicates.push(transactions[i]);
+        }
+      }
+
+    }
+    return duplicates;
+  }
+  private foundDuplicates(transaction: Transaction, transactionPool: Transaction[]): number {
+    return transactionPool.filter(t => t.amount === transaction.amount && t.payerReceiver === transaction.payerReceiver && t !== transaction).length
+  }
+  showDuplicates() {
+    this.expanded.reoccurent = !this.expanded.reoccurent;
+    this.duplicateTransactions = this.findDuplicateTransactions();
+
+  }
 }
