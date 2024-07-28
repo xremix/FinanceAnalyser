4caa37ad710c820dc4500687789fa7860301cbc0
xremix <tonidhoffmann@gmail.com>
2024-07-28 12:42:51 +0200

    improve categories

--- src/app/services/category-service.ts
+++ src/app/services/category-service.ts
@@ -25,10 +25,13 @@ export class CategoryService {
       if (this.matchesKeywords(category, transaction) && !this.matchesExcludeKeywords(category, transaction)) {
         return {category};
       }
-  
+      
       // Überprüfen Sie dann die Unterkategorien, falls vorhanden
       if (category.subCategories) {
         for (const subCategory of category.subCategories) {
+          if(transaction.payerReceiver.toLowerCase().includes('finanzamt') && category.name.toLowerCase().includes('steuer')){
+            debugger;
+          }
           if (this.matchesKeywords(subCategory, transaction) && !this.matchesExcludeKeywords(subCategory, transaction)) {
             return {category: subCategory, parentCategory: category};
           }
@@ -46,6 +49,12 @@ export class CategoryService {
   
   // Hilfsfunktion, um den Code sauber zu halten
   private matchesKeywords(category: Category, transaction: Transaction): boolean {
+    console.log('matched keywords', category.keywords.filter(
+      (keyword) =>
+        transaction.payerReceiver.toUpperCase().includes(keyword.toUpperCase()) ||
+        transaction.bookingText.toUpperCase().includes(keyword.toUpperCase()) ||
+        transaction.purpose.toUpperCase().includes(keyword.toUpperCase())
+    ));
     return category.keywords.some(
       (keyword) =>
         transaction.payerReceiver.toUpperCase().includes(keyword.toUpperCase()) ||
