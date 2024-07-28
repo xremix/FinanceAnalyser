09312b152b03f10901b0b8d29141cda8b3e9d30f
xremix <tonidhoffmann@gmail.com>
2024-07-28 15:21:57 +0200

    option to see monthly bilances without savings

--- src/app/monthy-billance/monthy-billance.component.ts
+++ src/app/monthy-billance/monthy-billance.component.ts
@@ -10,18 +10,30 @@ export class MonthyBillanceComponent implements OnInit {
   @Input() dates: Date[] = [];
   @Input() transactions: Transaction[] = [];
   public monthlyData: { name: string; income: number; expenses: number }[] = [];
+  public includeSavings = true;
   public get monthsWithPositiveBillance() {
     return this.monthlyData.filter((d) => d.income > d.expenses).length;
   }
   ngOnInit(): void {
+   this.refreshMonthlyData();
+  }
+
+  public toggleSavings() {
+    this.includeSavings = !this.includeSavings;
+    this.refreshMonthlyData();
+  }
+
+  public refreshMonthlyData(){
     this.monthlyData = this.dates.map((d) => {
       return {
         name: d.toLocaleDateString(),
         income: this.transactions
+          .filter(t => this.includeSavings || t.category?.name !== 'Sparen')
           .filter((t) => t.bookingDate.getMonth() === d.getMonth() && t.bookingDate.getFullYear() === d.getFullYear())
           .filter((t) => t.amount >= 0)
           .reduce((acc, t) => acc + (t.amount | 0), 0),
         expenses: this.transactions
+        .filter(t => this.includeSavings || t.category?.name !== 'Sparen')
           .filter((t) => t.bookingDate.getMonth() === d.getMonth() && t.bookingDate.getFullYear() === d.getFullYear())
           .filter((t) => t.amount < 0)
           .reduce((acc, t) => acc + ((t.amount * -1) | 0), 0),
